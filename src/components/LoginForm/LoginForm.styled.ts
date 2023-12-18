584c806dc566f6440f6eed53e6b3c622ce881816
sunub <bsc5672@gmail.com>
2023-12-18 23:07:56 +0900

    feat : Login 창을 구성하는 Login Form 생성

---
+++ src/components/LoginForm/LoginForm.styled.ts
@@ -0,0 +1,11 @@
+"use client";
+
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: grid;
+  place-content: center;
+  height: 100%;
+`;
+
+export { Wrapper };
---
+++ src/hooks/use-toggle.ts
@@ -0,0 +1,19 @@
+import React from "react";
+
+type ToggleReturn = [boolean, () => void];
+
+function useToggle(initValue = false): [boolean, () => void] {
+  if (typeof initValue !== "boolean") {
+    console.warn("useToggle의 인자값은 boolean이어야 합니다.");
+  }
+
+  const [isToggled, setToggled] = React.useState(initValue);
+
+  const executeToggle = React.useCallback(() => {
+    setToggled((prev) => !prev);
+  }, []);
+
+  return [isToggled, executeToggle];
+}
+
+export default useToggle;
