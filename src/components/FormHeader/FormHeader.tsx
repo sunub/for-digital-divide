855084c32576c268fe02db83a5c4d35405071fd5
sunub <bsc5672@gmail.com>
2023-12-18 23:07:10 +0900

    feat : 로그인 창 내의 로그인 방식과 어떤 로그인 창인지를 나타내는 LoginForm 추가

---
+++ src/components/FormHeader/FormHeader.tsx
@@ -0,0 +1,11 @@
+import * as Styled from "./FormHeader.styled";
+
+function FormHeader() {
+  return (
+    <Styled.Header>
+      <Styled.Title>로그인</Styled.Title>
+    </Styled.Header>
+  );
+}
+
+export default FormHeader;
