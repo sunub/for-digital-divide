8cf53d2e20c0d80be8a973e3f155aaf3b62589a4
sunub <bsc5672@gmail.com>
2024-02-23 23:47:49 +0900

    feat(app): WebAuthn을 사용하기 위한 PublicKey의 데이터를 요구하는 API
    세션에서 사용자의 정보를 불러오고 정보를 기반으로 navigator.credential API를 이용하여 WebAuthn에 필요한 옵션을 제공하는 역할을 한다.

---
+++ src/app/api/registerRequest/route.ts
@@ -0,0 +1,102 @@
+import { NextRequest, NextResponse } from "next/server";
+import { decode, encode } from "js-base64";
+import { Pool } from "pg";
+import User from "@/lib/fido/user";
+import { isoBase64URL } from "@simplewebauthn/server/helpers";
+import { generateRegistrationOptions } from "@simplewebauthn/server";
+import { cookies } from "next/headers";
+
+const RP_NAME = "WebAuthn Codelab";
+const TIMEOUT = 30 * 1000 * 60;
+
+export async function POST(req: NextRequest) {
+  const session = req.cookies.get("session")?.value;
+  // console.log(session);
+  const decodedSession = decode(session as string);
+  const parsedSession = JSON.parse(decodedSession);
+  const userId = decode(parsedSession.id);
+  const user = (await User.findByUserId(userId)).rows[0];
+  if (!user) {
+    return NextResponse.json(
+      { error: "사용자를 찾을 수 없습니다." },
+      { status: 400 },
+    );
+  }
+
+  const excludeCredentials: any[] = [];
+  if (user.credentials.length === 0) {
+    for (let cred of user.credentials) {
+      excludeCredentials.push({
+        id: isoBase64URL.toBuffer(cred.credId),
+        type: "public-key",
+        transports: ["internal"],
+      });
+    }
+  }
+
+  const pubKeyCredParams = [];
+  const params = [-7, -257];
+  for (let param of params) {
+    pubKeyCredParams.push({ type: "public-key", alg: param });
+  }
+
+  const res = await req.json();
+  const { opts } = res;
+
+  const as: any = {};
+  const aa = opts.authenticatorSelection.authenticatorAttachment;
+  const rr = opts.authenticatorSelection.requireResidentKey;
+  const uv = opts.authenticatorSelection.userVerification;
+  const cp = opts.attestation;
+  let asFlag = false;
+  let authenticatorSelection;
+  let attestation: AttestationConveyancePreference = "none";
+
+  if (aa && (aa == "platform" || aa == "cross-platform")) {
+    asFlag = true;
+    as.authenticatorAttachment = aa;
+  }
+  if (rr && typeof rr == "boolean") {
+    asFlag = true;
+    as.requireResidentKey = rr;
+  }
+  if (uv && (uv == "required" || uv == "preferred" || uv == "discouraged")) {
+    asFlag = true;
+    as.userVerification = uv;
+  }
+  if (asFlag) {
+    authenticatorSelection = as;
+  }
+  if (cp && (cp == "none" || cp == "indirect" || cp == "direct")) {
+    attestation = cp;
+  }
+
+  const rpId =
+    process.env.NODE_ENV === "production" ? process.env.HOSTNAME : "localhost";
+
+  const options = await generateRegistrationOptions({
+    rpName: RP_NAME,
+    rpID: rpId as string,
+    userID: user.id,
+    userName: user.username,
+    timeout: TIMEOUT,
+    // Prompt users for additional information about the authenticator.
+    attestationType: attestation,
+    // Prevent users from re-registering existing authenticators
+    excludeCredentials,
+    authenticatorSelection,
+  });
+
+  const challengeSession = {
+    ...parsedSession,
+    challenge: options.challenge,
+  };
+
+  cookies().set("session", encode(JSON.stringify(challengeSession)), {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+  });
+
+  return NextResponse.json({ options });
+}
