d340a85d5f1a83dd50f853a14261c7321803a83c
sunub <bsc5672@gmail.com>
2024-03-05 00:07:35 +0900

    fix(app): Server에서 document 관련 API를 사용하여 생기는 빌드 오류 수정

--- src/app/api/auth/signinResponse/route.ts
+++ src/app/api/auth/signinResponse/route.ts
@@ -1,9 +1,41 @@
-import User from "@/lib/fido/user";
-import { decode } from "js-base64";
-import { NextRequest, NextResponse } from "next/server";
-import { getOrigin } from "@lib/client";
-import { isoBase64URL } from "@simplewebauthn/server/helpers";
-import { verifyAuthenticationResponse } from "@simplewebauthn/server";
+'use server';
+
+import User from '@/lib/fido/user';
+import { NextRequest, NextResponse } from 'next/server';
+import { isoBase64URL } from '@simplewebauthn/server/helpers';
+import { verifyAuthenticationResponse } from '@simplewebauthn/server';
+
+const getOrigin = (userAgent: string): any => {
+  let origin = process.env.ORIGIN;
+
+  const appRe = /^[a-zA-z0-9_.]+/;
+  const match = userAgent.match(appRe);
+  if (match) {
+    // Check if UserAgent comes from a supported Android app.
+    if (process.env.ANDROID_PACKAGENAME && process.env.ANDROID_SHA256HASH) {
+      const package_names = process.env.ANDROID_PACKAGENAME.split(',').map(
+        (name) => name.trim(),
+      );
+      const hashes = process.env.ANDROID_SHA256HASH.split(',').map((hash) =>
+        hash.trim(),
+      );
+      const appName = match[0];
+      for (let i = 0; i < package_names.length; i++) {
+        if (appName === package_names[i]) {
+          // We recognize this app, so use the corresponding hash.
+          const octArray = hashes[i].split(':').map((h) => parseInt(h, 16));
+          const androidHash = isoBase64URL.fromBuffer(
+            octArray as unknown as Uint8Array,
+          );
+          origin = `android:apk-key-hash:${androidHash}`;
+          break;
+        }
+      }
+    }
+  }
+
+  return origin;
+};
 
 export async function POST(req: NextRequest) {
   const res = await req.json();
@@ -12,15 +44,15 @@ export async function POST(req: NextRequest) {
   const session = await User.getSession(req);
 
   const expectedChallenge = session.challenge;
-  const expectedOrigin = getOrigin(req.headers.get("user-agent")!);
+  const expectedOrigin = getOrigin(req.headers.get('User-Agent') as string);
   const expectedRPID =
-    process.env.NODE_ENV === "production" ? process.env.HOSTNAME : "localhost";
+    process.env.NODE_ENV === 'production' ? process.env.HOSTNAME : 'localhost';
 
   try {
     const cred = session.credential;
     if (!cred) {
       throw new Error(
-        "서버에 등록된 credential이 없습니다. 다시 로그인 해주세요",
+        '서버에 등록된 credential이 없습니다. 다시 로그인 해주세요',
       );
     }
 
@@ -35,14 +67,14 @@ export async function POST(req: NextRequest) {
       response: credential,
       expectedChallenge,
       expectedOrigin,
-      expectedRPID: expectedRPID || "",
+      expectedRPID: expectedRPID || '',
       authenticator,
       requireUserVerification: false,
     });
 
-    const { verified, authenticatorInfo } = verification;
+    const { verified } = verification;
     if (!verified) {
-      throw new Error("인증에 실패했습니다");
+      throw new Error('인증에 실패했습니다');
     }
 
     cred.last_used = new Date().getTime();
@@ -50,10 +82,8 @@ export async function POST(req: NextRequest) {
 
     await User.deleteChallenge(session.id);
 
-    return NextResponse.json({ message: "인증에 성공했습니다" });
+    return NextResponse.json({ message: '인증에 성공했습니다' });
   } catch (error) {
-    await User.deleteChallenge(session.id);
-
     console.error(error);
     return NextResponse.json({ error }, { status: 400 });
   }
