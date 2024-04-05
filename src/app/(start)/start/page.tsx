3945961dea1799d3716ef57ae3c47cc0a23eafd8
sunub <bsc5672@gmail.com>
2024-04-05 23:50:06 +0900

    feat(app)n : 초기에 사용자 이름을 추가하는 Modal 컴포넌트 추가

--- src/app/(start)/start/page.tsx
+++ src/app/(start)/start/page.tsx
@@ -6,6 +6,8 @@ import { NotificationContext } from '@/context/NotificationContext';
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
 import { ArrowIcon } from '@/icons';
+import Modal from '@/components/ui/modal';
+import Username from '@/components/LoginForm/LoginInput/Username';
 
 export default function Home() {
   const { action } = React.useContext(NotificationContext);
@@ -14,14 +16,36 @@ export default function Home() {
     const id = crypto.randomUUID();
     action.add({
       id,
-      message:
-        '안녕하세요. 저는 안내 메세지에요. 설명이 필요한 내용은 여기에 표시됩니다 :)',
+      message: `본인의 이름을 적어주셔도 되고 임의의 이름을 적어주셔도 되요! 이름을
+        적고 확인을 눌러 주세요!`,
       type: 'default',
     });
   }, []);
 
   return (
     <React.Fragment>
+      <div>
+        <Modal isOpen={true}>
+          <div className="flex flex-col items-center gap-2">
+            <h1 className="text-xl">
+              아래를 클릭 후 <b>키보드로</b> 입력해주세요!
+            </h1>
+            <ArrowIcon rotate={'rotate(-90deg)'} />
+          </div>
+          <Username
+            type="text"
+            name="username"
+            autoComplete="username"
+            minLength={1}
+            maxLength={40}
+            inputContent="사용자 이름을 입력해주세요"
+            labelContent="사용자 이름"
+            ariaLabel="사용자 이름 입력"
+            ariaLabelledby="사용자 이름 입력"
+            borderRadius={24}
+          />
+        </Modal>
+      </div>
       <Title>
         <h1>핸드폰을 클릭해주세요!</h1>
         <ArrowIcon />
