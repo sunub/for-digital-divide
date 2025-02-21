50f9039d1ce0e691c0042cde4afae6ce4a086bb2
sunub <bsc5672@gmail.com>
2025-02-21 16:43:08 +0900

    feat : zustand를 재사용 할 수 있게끔 훅 추가

--- src/context/NotificationContext.tsx
+++ src/context/NotificationContext.tsx
@@ -1,40 +1,13 @@
 'use client';
 
-import React from 'react';
-import { type StoreApi, useStore } from 'zustand';
+import { useStore } from 'zustand';
 import {
   type NotificationStore,
-  createNotificationStore,
+  notificationStore,
 } from '@/store/notification-store';
-
-export const NotificationContext =
-  React.createContext<StoreApi<NotificationStore> | null>(null);
-
-export function NotificationProvider({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  const notificationRef = React.useRef<StoreApi<NotificationStore>>();
-  if (!notificationRef.current) {
-    notificationRef.current = createNotificationStore();
-  }
-
-  return (
-    <NotificationContext.Provider value={notificationRef.current}>
-      {children}
-    </NotificationContext.Provider>
-  );
-}
 
 export const useNotificationStore = <T,>(
   selector: (store: NotificationStore) => T,
 ): T => {
-  const notificationContext = React.useContext(NotificationContext);
-  if (!notificationContext) {
-    throw new Error(
-      'useNotificationStore must be used within a NotificationContextProvider',
-    );
-  }
-  return useStore(notificationContext, selector);
+  return useStore(notificationStore, selector);
 };
