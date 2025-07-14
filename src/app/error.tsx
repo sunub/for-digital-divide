bfb0deeb45e41782c6c3549abe4140b214105934
sunub <bsc5672@gmail.com>
2025-07-14 12:33:59 +0900

    feat : Error 페이지를 단순한 에러 페이지 보다 스타일을 적용하여 제작

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,31 +1,21 @@
 'use client';
 
 import React from 'react';
-import { Button } from '@/components/ui/status-button';
 import { goToHome } from '@/utils/revalidate';
+import Button from '@/components/Button';
 
-export default function Error({
-  error,
-  reset,
-}: {
-  error: Error & { digest?: string };
-  reset: () => void;
-}) {
+export default function Error({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
   React.useEffect(() => {
     console.error(error);
   }, [error]);
 
   return (
-    <div className="absolute top-0 left-0 w-[100dvw] h-[100dvh] bg-gradient-radial from-[oklch(88.45%,0.059,18.33)] to-[oklch(71.42%,0.059,18.33)]">
+    <div className="fixed top-0 left-0 w-[100dvw] h-[100dvh] bg-gradient-radial from-[oklch(88.45%,0.059,18.33)] to-[oklch(71.42%,0.059,18.33)] z-50">
       <div className="bg-red-200 bg-error-pattern animate-pan w-full h-full bg-[10%] flex items-center justify-center">
         <div className="bg-slate-100 p-12 flex flex-col rounded-xl place-content-center w-full h-fit">
-          <h1 className="text-2xl font-bold ring-offset-1 leading-relaxed">
-            {error.message}
-          </h1>
+          <h1 className="text-2xl font-bold ring-offset-1 leading-relaxed">{error.message}</h1>
           <div className="mt-5 mb-10">
-            <pre className="overflow-hidden text-ellipsis whitespace-nowrap w-full">
-              {error.stack?.split('\n')[0]}
-            </pre>
+            <pre className="overflow-hidden text-ellipsis whitespace-nowrap w-full">{error.stack?.split('\n')[0]}</pre>
             <p>{error.digest}</p>
           </div>
           <div className="flex flex-row gap-2">
