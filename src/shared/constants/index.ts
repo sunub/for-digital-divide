e686dfd5f9e3d9c49ebcc93f6fb1ba304cfb91ad
sunub <bsc5672@gmail.com>
2025-07-14 14:14:59 +0900

    feat : 리다이렉트 및 Stepper를 위한 Constant 작성

---
+++ src/shared/constants/index.ts
@@ -0,0 +1,23 @@
+export const SITE_MAP = [
+  {
+    step: 1,
+    title: 'start',
+    path: '/',
+  },
+  {
+    step: 2,
+    title: 'intro',
+    path: '/intro',
+  },
+  {
+    step: 3,
+    title: '회원가입',
+    path: '/signup',
+  },
+];
+
+export const REDIRECT_REASONS = {
+  ALREADY_REGISTERED: 'already-registered',
+  EMAIL_NOT_VERIFIED: 'email-not-verified',
+  PIN_NOT_VERIFIED: 'pin-not-verified',
+} as const;
