5e72357f6f597a222e0cf50084f666b632250c2f
sunub <bsc5672@gmail.com>
2025-07-14 12:38:06 +0900

    refactor : 개발 편의성을 위해 Reload Button 추가 및 UX 개선을 위한 Stepper 및 Jotai 도입
    - 개발 편의성과 빠른 UI 테스트를 위한 ReloadButton 추가
    - 전역 상태 관리를 위한 JotaiProvider 도입
    - 단계별 진행상황 안내를 위한 Stepper 컴포넌트 배치
    - Gugi 웹폰트 적용으로 브랜드 아이덴티티 및 시각적 일관성 강화
    - 전체적으로 유지보수성과 사용자 경험, 개발 생산성을 모두 고려한 구조 개선

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,35 @@
-import React from 'react';
 import './globals.css';
-import StyledComponentsRegistry from '@/components/StyledComponentsRegistry';
-import NotificationList from '@/components/NotificationList';
+import React from 'react';
+import { Gugi } from 'next/font/google';
 import { ReloadButton } from '@/components/ReloadButton';
+import { Stepper } from '@/components/Stepper/ui/Stepper';
+import { JotaiProvider } from '@/provider/JotaiProvider';
+import { ToastContainer } from '@/provider/toast/ui/ToastContainer';
+import StyledComponentsRegistry from '@/components/StyledComponentsRegistry';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+const gugi = Gugi({ subsets: ['latin'], weight: '400' });
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="kor">
+    <html lang="kor" className={gugi.className}>
       <head>
-        <link
-          href="https://fonts.googleapis.com/icon?family=Material+Icons"
-          rel="stylesheet preload"
-          as="style"
-        />
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet preload" as="style" />
         <meta charSet="utf-8" />
         <meta httpEquiv="u-ea-compatible" content="IE=edge" />
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </head>
       <body id="root">
         <StyledComponentsRegistry>
-          <div id="_next">
-            <div id="devsite-content">{children}</div>
-            <ReloadButton />
-          </div>
-          <NotificationList />
+          <JotaiProvider>
+            <div id="_next">
+              <div id="devsite-content">
+                <Stepper />
+                {children}
+              </div>
+              <ReloadButton />
+            </div>
+            <ToastContainer />
+          </JotaiProvider>
         </StyledComponentsRegistry>
       </body>
     </html>
