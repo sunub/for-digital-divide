2d3d43a9099048165cb9172f03c3cc5beb79826a
sunub <bsc5672@gmail.com>
2025-07-15 19:17:34 +0900

    feat(schema) : UsersSchema의 session_id를 nullable로 수정

--- src/entities/users/users.model.ts
+++ src/entities/users/users.model.ts
@@ -7,7 +7,7 @@ export const UsersIdSchema = serial;
 export const UsersSchema = z.object({
   user_id: serial,
   name: z.string().min(1).max(100),
-  session_id: z.string().min(1).max(100),
+  session_id: z.string().min(1).max(100).nullable(),
   email: z.email(),
   created_at: z.date().optional(),
 });
