9f49cc28e4f2891fb824b743cd56ac630a507db4
sunub <bsc5672@gmail.com>
2025-07-17 01:42:24 +0900

    refactor : 리펙토링에 맞춰 로직 수정

--- src/app/login/email-password/hooks/useSeedingDemoData.ts
+++ src/app/login/email-password/hooks/useSeedingDemoData.ts
@@ -1,12 +1,12 @@
 import { useEffect } from 'react';
-import { seedDemoAccountInfo } from '@root/prisma/seed';
+import { seedDemoAccountAndTransactionInfo } from '@root/prisma/seed';
 
 export function useSeedingDemoData(isSeedingProgress: boolean, onSeedingComplete: (completed: boolean) => void) {
   useEffect(() => {
     if (isSeedingProgress) {
       (async () => {
         console.log('데모 데이터 심고 있는 중~~~~~~~~');
-        await seedDemoAccountInfo();
+        await seedDemoAccountAndTransactionInfo();
         onSeedingComplete(true);
       })();
     }
