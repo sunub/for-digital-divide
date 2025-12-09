b1dafed1e5a3b8c671a8f22bdc21fdad03141ab4
sunub <bsc5672@gmail.com>
2025-12-10 00:43:06 +0900

    feat: Flex, Grid, Size 스타일 파일 추가 및 기본 스타일 정의

---
+++ src/style/Size.css.ts
@@ -0,0 +1,11 @@
+import { style } from "@vanilla-extract/css";
+
+export const FullDeviceViewportSize = style({
+  width: "100dvw",
+  height: "100dvh",
+});
+
+export const FullSize = style({
+  width: "100%",
+  height: "100%",
+});
