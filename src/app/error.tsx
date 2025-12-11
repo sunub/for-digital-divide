57eb5611bf219e3af3f623bcf081e561d9b54584
sunub <bsc5672@gmail.com>
2025-12-12 00:39:44 +0900

    feat: 에러 페이지 스타일을 Vanilla Extract로 재구성 및 기존 스타일 제거

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,25 +1,29 @@
-'use client';
+"use client";
 
-import React from 'react';
-import { goToHome } from '@/utils/revalidate';
-import Button from '@/components/Button';
+import Button from "@/components/3DButton";
+import { goToHome } from "@/utils/revalidate";
+import * as styles from "./error.css";
 
-export default function Error({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
-  React.useEffect(() => {
-    console.error(error);
-  }, [error]);
-
+export default function ErrorPage({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
   return (
-    <div className="fixed top-0 left-0 w-[100dvw] h-[100dvh] bg-gradient-radial from-[oklch(88.45%,0.059,18.33)] to-[oklch(71.42%,0.059,18.33)] z-50">
-      <div className="bg-red-200 bg-error-pattern animate-pan w-full h-full bg-[10%] flex items-center justify-center">
-        <div className="bg-slate-100 p-12 flex flex-col rounded-xl place-content-center w-full h-fit">
-          <h1 className="text-2xl font-bold ring-offset-1 leading-relaxed">{error.message}</h1>
-          <div className="mt-5 mb-10">
-            <pre className="overflow-hidden text-ellipsis whitespace-nowrap w-full">{error.stack?.split('\n')[0]}</pre>
+    <div className={styles.container}>
+      <div className={styles.patternWrapper}>
+        <div className={styles.card}>
+          <h1 className={styles.heading}>{error.message}</h1>
+          <div className={styles.detailsContainer}>
+            <pre className={styles.codeBlock}>
+              {error.stack?.split("\n")[0]}
+            </pre>
             <p>{error.digest}</p>
           </div>
-          <div className="flex flex-row gap-2">
-            <Button variant={'destructive'} onClick={() => goToHome()}>
+          <div className={styles.buttonGroup}>
+            <Button variant={"destructive"} onClick={() => goToHome()}>
               홈으로 돌아가기
             </Button>
             <Button onClick={reset}>다시 시도해주세요</Button>
