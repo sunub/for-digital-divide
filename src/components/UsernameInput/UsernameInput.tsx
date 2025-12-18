f398b8dc52ff986162a6e54e12ff53f602e718b9
sunub <bsc5672@gmail.com>
2025-12-19 00:19:01 +0900

    feat(components/UsernameInput): 사용자명 입력 컴포넌트 및 유효성 검사 훅 추가
    - UsernameInput 컴포넌트 구현
    - useUsernameValidation 커스텀 훅 추가
    - index.ts 엔트리포인트 구성

---
+++ src/components/UsernameInput/UsernameInput.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { UserIcon } from "lucide-react";
+import { useState } from "react";
+import { TextField } from "@/components/TextField/TextField";
+import { Flex } from "@/shared/ui/Flex";
+import { errorText } from "../EmailAndPasswordField/EmailAndPasswordField.css";
+import { useUsernameValidation } from "./hooks/useUsernameValidation";
+
+export function UsernameInput() {
+  const [value, setValue] = useState("");
+  const [usernameError, validateUsername] = useUsernameValidation();
+
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.target.value;
+    setValue(newValue);
+  };
+
+  const handleOnBlur = () => {
+    validateUsername(value);
+  };
+
+  return (
+    <Flex direction={"column"}>
+      <TextField
+        id="register-username"
+        value={value}
+        name="username"
+        onBlur={handleOnBlur}
+        onChange={handleOnChange}
+        isError={!usernameError}
+        labelContent="사용자 이름을 입력해주세요"
+        autoComplete="username"
+        leftIcon={<UserIcon size={16} />}
+      />
+      <span className={errorText({ isVisible: usernameError != null })}>
+        {usernameError ||
+          "2자 이상 20자 이하의 한글 또는 영문 대소문자만 사용할 수 있습니다."}
+      </span>
+    </Flex>
+  );
+}
