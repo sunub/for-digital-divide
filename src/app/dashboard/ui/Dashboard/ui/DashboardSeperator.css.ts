9b4f34c681a2a50fa95eebec60a68d24acf3a153
sunub <bsc5672@gmail.com>
2025-12-22 19:22:09 +0900

    feat : Dashboard 내에서 컨텐츠를 구분하기 위한 Seperator 구현

---
+++ src/app/dashboard/ui/Dashboard/ui/DashboardSeperator.css.ts
@@ -0,0 +1,15 @@
+import { style } from "@vanilla-extract/css";
+
+export const seperator = style({
+  height: "5px",
+  width: "20cqw",
+  borderRadius: "10px",
+  backgroundColor:
+    "color-mix(in oklch, oklch(0.6463 0.1926 279.56) 70%, oklch(0.7 0.1825 239.69) 5%)",
+  opacity: 0.15,
+
+  marginRight: "auto",
+  marginLeft: "auto",
+  marginTop: "1.25rem",
+  marginBottom: "2rem",
+});
---
+++ src/app/dashboard/ui/Dashboard/ui/DashboardSeperator.tsx
@@ -0,0 +1,5 @@
+import * as style from "./DashboardSeperator.css";
+
+export function DashboardSeperator() {
+  return <div className={style.seperator} />;
+}
