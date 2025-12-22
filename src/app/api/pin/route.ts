5f1002b1d4642effebeaead001393381b81ab33d
sunub <bsc5672@gmail.com>
2025-12-22 20:05:00 +0900

    refactor : 불필요한 route 파일 제거

--- src/app/api/pin/route.ts
+++
@@ -1,6 +0,0 @@
-import { type NextRequest, NextResponse } from "next/server";
-
-export function POST(req: NextRequest) {
-  console.log("Request body:", req.body);
-  return NextResponse.json({ message: "Hello, World!" });
-}
