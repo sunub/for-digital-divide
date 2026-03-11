03e59812f11128eef31a6b7e5083370929041ca4
sunub <bsc5672@gmail.com>
2026-03-11 17:43:53 +0900

    chore : 모노레포 환경에서 디자인 시스템을 적용하기 위한 환경설정 추가

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,7 @@ const withVanillaExtract = createVanillaExtractPlugin();
 const nextConfig: NextConfig = {
   basePath: "",
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+  transpilePackages: ["@for-digital-divide/design-system"],
   images: {
     formats: ["image/avif", "image/webp"],
   },
