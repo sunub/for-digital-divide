92ee8de607d6926825443dc462eb4ce81763c1ca
sunub <bsc5672@gmail.com>
2026-07-16 10:32:46 +0900

    fix : preftech 시 데이터를 await 하지 않아 의미를 소실한 문제 개선

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -35,7 +35,7 @@ export default async function Dashboard() {
   const defaultAccount = accounts[0];
 
   if (defaultAccount) {
-    queryClient.prefetchQuery({
+    await queryClient.prefetchQuery({
       queryKey: transactionKeys.byAccount(defaultAccount.account_number),
       queryFn: () => getTransactions(defaultAccount.account_number),
     });
